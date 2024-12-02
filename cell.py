02adc0c6a6173fd73974378784f763fbce63e712
Pranav Marepalli <marepallipranav108@gmail.com>
2024-12-02 18:17:59 -0500

    finished cell.py hopefully works

--- cell.py
+++ cell.py
@@ -19,3 +19,31 @@ class Cell:
     def set_sketched_value(self, value):
         self.sketchval=value
 
+
+    def draw(self):
+        width=self.screen.get_width()//9
+        length=self.screen.get_height()//9
+        pygame.draw.rect(self.screen, self.outline_col, (width*self.col, row*length), 2)
+
+    
+        game_font=pygame.font.Font(None, 30)
+
+        if self.value!=0:
+            tex = game_font.render(str(self.value), True, BLACK)
+            self.screen.blit(tex, ((self.col*width)+10, (self.row*length)+10))
+        elif self.sketchval!=0:
+            tex=game_font(str(self.sketchval), True, RED)
+            self.screen.blit(tex, ((self.col*width+5), (self.row*length)+5))
+
+    
+    def restart(self):
+        self.value=0
+        self.sketchval=0
+
+        
+
+
+
+
+
+    
